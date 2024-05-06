496edb5012ff4fa6b237f623a819e828001009cf
Rohit Raj <rohitraj3552008@gmail.com>
2024-05-06 15:01:25 +0530

    Update Py_data_QC.py

--- Bike_stores/Py_data_QC.py
+++ Bike_stores/Py_data_QC.py
@@ -34,7 +34,49 @@ for col in range(1,ws.max_column+1):
 if f == 0:
     print ("Data file has all the necessary information")
 
+#logical checks
+for col in range(1,ws.max_column+1):
+    if col == "order_item_quantity":
+        order_quantity = get_column_letter(col)
+    elif col == "store_quantity":
+        store_quantity = get_column_letter(col)
+    elif col == "order_status":
+        order_status = get_column_letter(col)
+    elif col == "order_id":
+        order_id = get_column_letter(col)
+    elif col == "product_name":
+        product_name = get_column_letter(col)
+for row in range(2,ws.max_row+1):
+    if ws[order_quantity + str(1)].value > ws[store_quantity + str(1)].value or ws[order_status + str(row)].value != 4:
+        print ("Order cannot be completed for " + ws[order_id + str(1)].value + " ordering " + ws[product_name + str(1)].value)
 
-#column sum checks
 #range checks
+for col in range(1,ws.max_column+1):
+    if col == "discount":
+        discount = get_column_letter(col)
+    elif col == "product_id":
+        product_id = get_column_letter(col)
+for row in range(2,ws.max_row+1):
+    if ws[discount + str(1)].value > 1:
+        print ("Incorrect discount for the product " + ws[product_id + str(1)].value + " is given")
+
+
 #text or number checks
+f = 0
+num_list = ["product_id", "brand_id", "category_id", "model_year", "product_list_price", "order_id", "item_id", "order_item_quantity", "order_item_list_price", "discount", "customer_id", "order_status", "store_id", "staff_id", "customer_zip_code", "store_zip_code", "active", "manager_id", "store_quantity"]
+for col in range(1,ws.max_column+1):
+    for row in range(2,ws.max_row+1):
+        char = get_column_letter(col)
+        if ws[char + str(row)].value is not None:
+            if ws[char + str(1)].value in num_list and isinstance(ws[char + str(row)].value,(int, float)) == False:
+                print ("Data file has incorrect data types")
+                f = 1
+                break
+            elif ws[char + str(1)].value in num_list and isinstance(ws[char + str(row)].value,str) == False:
+                print ("Data file has incorrect data types")
+                f = 1
+                break
+    if f == 1:
+        break
+if f == 0:
+    print ("Data file has correct datatypes")
